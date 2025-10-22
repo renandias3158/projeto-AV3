f194dc3da9c7e59eb02e4650b231e76f335877ce
Renan Aprigio Dias de Moura <renan.dias3158@gmail.com>
2025-10-22 07:31:31 -0300

    THE commit

---
+++ modelo.py
@@ -0,0 +1,11 @@
+import pickle
+import numpy as np
+
+
+with open('modelo_dt.pkl') as f:
+    modelo = pickle.load(f)
+
+def prever(dados):
+    entrada = np.array([dados])
+    resultado = modelo.predict(entrada)
+    return resultado[0]
